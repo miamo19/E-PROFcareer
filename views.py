2c7d71f6b6e4072360569a6dcaca9176c33d33fe
Hyacinthe <hyacinthemiamo5@gmail.com>
2023-02-07 11:21:37 +0100

    Update views.py

--- views.py
+++ views.py
@@ -5,17 +5,22 @@ from django.shortcuts import render
 def index(request):
     return render (request, 'etudiant/index.html')
 
+
 def login(request):
     return render(request, 'etudiant/login.html')
+
 
 def about(request):
     return render(request, 'etudiant/about.html')
 
+
 def course(request):
     return render(request, 'etudiant/course.html')
 
+
 def contact(request):
     return render(request, 'etudiant/contact.html')
+
 
 def event(request):
     return render(request, 'etudiant/event.html')
